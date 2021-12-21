263719e47397694696f39407c4ec21acec7fc593
emanuelchristo <emanuelchristochris@gmail.com>
2021-12-21 11:29:58 +0530

    404 page center

--- pages/404.js
+++ pages/404.js
@@ -1,25 +1,14 @@
-import Link from 'next/link';
-import styles from '../styles/404.module.css';
+import Link from 'next/link'
 export default function Custom404() {
-  return (
-    <main>
-      <div className={styles["container-404"]}>
-        <div className={styles['title-404']}>
-          <h1>It&apos;s 404 baby😏</h1>
-        </div>
-        <iframe
-          src="https://giphy.com/embed/5nmobhwPiNsKELWk69"
-          width="480"
-          height="362"
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
-        <div className={styles['go-home-404']}>
-          <Link href="/" passHref>
-            <button>Go home</button>
-          </Link>
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div className='vh-100 flex-center flex-col'>
+				<h1>It&apos;s 404 baby😏</h1>
+				<iframe src='https://giphy.com/embed/5nmobhwPiNsKELWk69' width='480' height='362' frameBorder='0' allowFullScreen></iframe>
+				<Link href='/' passHref>
+					<button>Go home</button>
+				</Link>
+			</div>
+		</main>
+	)
 }
