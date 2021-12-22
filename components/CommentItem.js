73d0532aee481c7952c4f87bf64c25b767fc2af3
emanuelchristo <emanuelchristochris@gmail.com>
2021-12-22 19:20:09 +0530

    username/slug page

--- components/CommentItem.js
+++ components/CommentItem.js
@@ -1,41 +1,46 @@
-
-import Link from 'next/link';
+import Link from 'next/link'
 import styles from '../styles/CommentItem.module.css'
-import {firestore, auth} from '../lib/firebase'
-import {AiOutlineDelete} from 'react-icons/ai'
+import { firestore, auth } from '../lib/firebase'
+import { AiOutlineDelete } from 'react-icons/ai'
 
-function DeleteComment({comment, postRef, rerender})
-{
-	const del = async (e)=>{
-		e.currentTarget.onClick = () => {return}
+function DeleteComment({ comment, postRef, rerender }) {
+	const del = async (e) => {
+		e.currentTarget.onClick = () => {
+			return
+		}
 		//const currentComment = document.getElementById(comment.commentId)
 		//currentComment.parentNode.removeChild(currentComment)
-		const batch = firestore.batch();
-        batch.delete(postRef.collection('comments').doc(comment.commentId));
-		console.log("going to delete comment")
-        await batch.commit();
+		const batch = firestore.batch()
+		batch.delete(postRef.collection('comments').doc(comment.commentId))
+		console.log('going to delete comment')
+		await batch.commit()
 		rerender.setRerender(!rerender.rerender)
-
 	}
 	return (
-	
-	<div onClick={del}> <AiOutlineDelete/> </div>
+		<div onClick={del}>
+			{' '}
+			<AiOutlineDelete />{' '}
+		</div>
 	)
 }
-function CommentItem({ comment, postRef , rerender}) {
-	const uid = auth.currentUser? auth.currentUser.uid:null
+function CommentItem({ comment, postRef, rerender }) {
+	const uid = auth.currentUser ? auth.currentUser.uid : null
 	return (
-		<div id = {comment.commentId} className={styles['comment-container']}>
+		<div id={comment.commentId} className={styles['comment-container']}>
 			<div className={styles['comment-wrapper']}>
-
-				<Link href={`/${comment.username}`} passHref>
-					<a className={styles['author']}>@{comment.username}</a>
-				</Link>
-				<div >{(uid===comment.uid)? <DeleteComment comment = {comment} postRef={postRef} rerender ={rerender}/>:``}</div>
+				<div className='flex align-center'>
+					<Link href={`/${comment.username}`} passHref>
+						<a className={styles['author']}>@{comment.username}</a>
+					</Link>
+					<div className='spacerh-xs'></div>
+					<div className='pointer'>
+						{uid === comment.uid ? <DeleteComment comment={comment} postRef={postRef} rerender={rerender} /> : ``}
+					</div>
+				</div>
 				<p className={styles['comment-content']}>{comment.content}</p>
 			</div>
 		</div>
 	)
 }
 
-export default CommentItem;
+export default CommentItem
--- components/PostContent.js
+++ components/PostContent.js
@@ -1,211 +1,156 @@
-
-import dynamic from 'next/dynamic';
-const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
-import 'react-quill/dist/quill.bubble.css';
-import CommentItem from './CommentItem';
+import dynamic from 'next/dynamic'
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false })
+import 'react-quill/dist/quill.bubble.css'
+import CommentItem from './CommentItem'
 
 // extras
-import React ,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react'
 import styles from '../styles/PostContent.module.css'
-import  Share  from './Share';
-import  InfoDots  from './InfoDots';
-import PostItem from './PostItem';
+import Share from './Share'
+import InfoDots from './InfoDots'
+import PostItem from './PostItem'
+import PostFeed from './PostFeed'
 
+import LikeButton from './LikeButton'
+import AuthCheck from './AuthCheck'
+import Link from 'next/dist/client/link'
 
-import LikeButton from './LikeButton';
-import AuthCheck from './AuthCheck';
-import Link from "next/dist/client/link";
+import { AiOutlineLike } from 'react-icons/ai'
 
-import { AiOutlineLike } from 'react-icons/ai';
-
-import CommentButton from "./CommentButton"
-import { useCollection } from 'react-firebase-hooks/firestore';
+import CommentButton from './CommentButton'
+import { useCollection } from 'react-firebase-hooks/firestore'
 
 // sample dummy comment
-let sample_comments = [{username :"john123",content : "This is such an insightful post!" }, {username :"Joe11",content : "Wonderful, keep up the great work." }]
-function removeElementsByClass(className){
-    className = styles[className]
-	var elements = document.getElementsByClassName(className);
-    while(elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0]);
-    }
+let sample_comments = [
+	{ username: 'john123', content: 'This is such an insightful post!' },
+	{ username: 'Joe11', content: 'Wonderful, keep up the great work.' },
+]
+function removeElementsByClass(className) {
+	className = styles[className]
+	var elements = document.getElementsByClassName(className)
+	while (elements.length > 0) {
+		elements[0].parentNode.removeChild(elements[0])
+	}
 }
 
-const displayLoginPrompt= (e ,text) => {
-	
+const displayLoginPrompt = (e, text) => {
 	removeElementsByClass('please-login-container')
 	const mainDiv = document.getElementById('main-container')
-	const newSubDiv = document.createElement("div");
+	const newSubDiv = document.createElement('div')
 	newSubDiv.className = styles['please-login-container']
 	newSubDiv.innerHTML = `<a href="/auth"> <button>${text}</button></a>`
 	mainDiv.appendChild(newSubDiv)
-	
-	
+
 	//const target = `<div className={styles['signup-container']}><Link href="/auth">Please Sign In</Link></div>`
 	//debugger
 }
 
-
 let allComments = []
-let uid =0
+let uid = 0
 
 const PostContent = ({ post, posts, postRef }) => {
-    const createdAt = typeof post?.createdAt === 'number' ? new Date(post.createdAt) : post.createdAt.toDate();
+	const createdAt = typeof post?.createdAt === 'number' ? new Date(post.createdAt) : post.createdAt.toDate()
 	const [comments, commentsLoading, commentsError] = useCollection(postRef.collection('comments').orderBy('createdAt'))
 	const [rerender, setRerender] = useState(false)
-	useEffect(()=>{
-		if (comments)
-		{
+	useEffect(() => {
+		if (comments) {
 			allComments = []
-			comments.forEach((doc)=>{
+			comments.forEach((doc) => {
 				const commentData = doc.data()
-				allComments.push(
-					{
-						username : commentData.username,
-						content : commentData.content,
-						uid : commentData.uid,
-						commentId : doc.id
-						
-					}
-				)
-				
+				allComments.push({
+					username: commentData.username,
+					content: commentData.content,
+					uid: commentData.uid,
+					commentId: doc.id,
+				})
 			})
-		}	
+		}
 	}, [comments, rerender])
-	
-	
-	 
 
-    return (
-		<div id = 'main-container'className={styles['posts-section']}>
+	return (
+		<div id='main-container' className={styles['posts-section'] + ' margin'}>
+			<div className='nav-spacer'></div>
+			<div className='spacerv-sm'></div>
 			<div className={styles['posts-container']}>
-
-				<div className={styles["heading"]}>
-
+				<div className={styles['heading']}>
 					<Link href={`/${post.username}/${post.slug}`} passHref>
-							<h3 style={{ cursor: 'pointer' }} >
-								{post.title}
-							</h3>
+						<h2 style={{ cursor: 'pointer' }}>{post.title}</h2>
 					</Link>
 				</div>
-				<div className={styles['mini-description']}>
-					Add Mini description here
-				</div>
+				<div className={styles['mini-description']}>Add Mini description here</div>
 				<div className={styles['post-wrapper']}>
 					By &nbsp;
 					<Link href={`/${post.username}`} passHref>
 						<a className={styles['author']}>@{post.username}</a>
-					</Link> &nbsp;
-					
-					on {createdAt.toDateString()}
-
-
+					</Link>{' '}
+					&nbsp; on {createdAt.toDateString()}
 				</div>
-				<div className = {styles['icons']}>
-					<div  >	<AuthCheck
-							fallback={
-								<div onClick={(e)=>{
-									displayLoginPrompt(e,"Please login to Like")
-								}}><AiOutlineLike />  </div>
-							}>
-						<LikeButton postRef={postRef}/>
-						</AuthCheck> </div> <div>{post.likeCount}</div><Share/> <InfoDots/>
-				</div>
-				
-				<div className= {styles['content']}>
-
-					<ReactQuill value={post?.content} readOnly={true} theme={"bubble"} />	
-
-				</div>
-
-				<div className={styles['comments']}>
-					<h3>Comments</h3>
-					<div>
-						<label><h6>Add a comment</h6></label>
-						<input id="comment-box" type="text" placeholder="Add a comment" />
+				<div className={styles['icons']}>
+					<div className={styles['like-icon'] + ' pointer'}>
 						<AuthCheck
 							fallback={
-								
-								<div onClick={(e)=>{
-									displayLoginPrompt(e,"Please login to Comment")
-								}}> <button id ='add-comment-button'>Add Comment</button>  </div>
-							}> <CommentButton postRef={postRef} rerender = {{rerender :rerender ,setRerender: setRerender}}/> </AuthCheck>
+								<div
+									onClick={(e) => {
+										displayLoginPrompt(e, 'Please login to Like')
+									}}
+								>
+									<AiOutlineLike />
+								</div>
+							}
+						>
+							<LikeButton postRef={postRef} />
+						</AuthCheck>
 					</div>
-					<div id = "all-comments-container">{ allComments ? allComments.map((comment) => <CommentItem key={uid++} comment={comment}  postRef={postRef}  rerender = {{rerender :rerender ,setRerender: setRerender}}/>): `` }</div>
-
+					<div className={styles['like-count']}>{post.likeCount}</div>
+					<Share />
+					<InfoDots />
 				</div>
 
+				<div className={styles['content'] + ' blog-margin'}>
+					<ReactQuill value={post?.content} readOnly={true} theme={'bubble'} />
+				</div>
 
+				<div className='spacerv-sm'></div>
+				<div className={styles['comments'] + ' blog-margin'}>
+					<h3>Comments</h3>
+					<div className='flex align-center'>
+						<input id='comment-box' type='text' placeholder='Add a comment' />
+						<div className='spacerh-xs'></div>
+						<AuthCheck
+							fallback={
+								<div
+									onClick={(e) => {
+										displayLoginPrompt(e, 'Please login to Comment')
+									}}
+								>
+									<button id='add-comment-button'>Add Comment</button>
+								</div>
+							}
+						>
+							<CommentButton postRef={postRef} rerender={{ rerender: rerender, setRerender: setRerender }} />
+						</AuthCheck>
+					</div>
+					<div id='all-comments-container'>
+						{allComments
+							? allComments.map((comment) => (
+									<CommentItem
+										key={uid++}
+										comment={comment}
+										postRef={postRef}
+										rerender={{ rerender: rerender, setRerender: setRerender }}
+									/>
+							  ))
+							: ``}
+					</div>
+				</div>
+				<div className='spacerv-sm'></div>
 			</div>
 
-			
-			<div>
-			<div className={styles['cards-container']}>{posts ? posts.map((post) => <PostItem key={uid++} post={post}  />) : null}</div>
-				
-			</div>
-
-
-		</div>
-
-
-        /*
-        <div>
-            <h1>{post?.title}</h1>
-            <span>
-                Authored by
-                <Link href={`/${post.username}/`} passHref>
-                    <a>@{post.username}</a>
-                </Link>
-                {createdAt.toISOString()}
-            </span>
-            <ReactQuill value={post?.content}
-                readOnly={true} theme={"bubble"} />
-        </div>
-        */
-    );
-}
-// comments need : uid, username, content, createdAt,  
-export default PostContent;
-
-
-
-//Admin props for editing the post authored by the currently logged in user
-function PostFeed({ posts, admin }) {
-	return (
-		<section className={styles['posts-section'] + ' margin'}>
-			<div className="spacer-m"></div>
-			<h2>Recent Posts</h2>
-			<div className={styles['posts-container']}>{posts ? posts.map((post) => <PostItem post={post} key={post.slug} admin={admin} />) : null}</div>
-			<div className="spacer-m"></div>
-		</section>
-	)
-}
-/*
-function PostItem({ post }) {
-	//Show the word count in post card.
-	const wordCount = post?.content.trim().split(/\s+/g).length
-	const minutesToRead = (wordCount / 100 + 1).toFixed(0)
-
-	return (
-		<div className={styles['post-container']}>
-			<div className={styles['post-wrapper']}>
-				<div className={styles['image']}></div>
-				<Link href={`/${post.username}`} passHref>
-					<a className={styles['author']}>@{post.username}</a>
-				</Link>
-				<Link href={`/${post.username}/${post.slug}`} passHref>
-					<h3 style={{ cursor: 'pointer' }} className={styles.title}>
-						{post.title}
-					</h3>
-				</Link>
-				<p className={styles['post-info']}>
-					{wordCount} word{wordCount > 1 ? 's' : ''} — {minutesToRead} min read
-					
-				</p>
-				
-				
-			</div>
+			<div className='spacerv-sm'></div>
+			<PostFeed title='More posts' posts={posts} admin={false} />
+			<div className='spacerv-md'></div>
 		</div>
 	)
 }
-*/
+// comments need : uid, username, content, createdAt,
+export default PostContent
