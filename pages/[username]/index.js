8627bd542ea14d2c40cc77f202a08de98f6e0103
Amal Prakash <amalkj20000@gmail.com>
2022-03-31 14:36:24 +0530

    Unpublished Posts (All Posts) Added to Your Profile

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,10 +1,12 @@
 import { auth, firestore, getUidFromUsername ,getUserWithUsername, postToJSON } from "../../lib/firebase";
 import AuthCheck from "../../components/AuthCheck";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 import router from "next/router";
 import { useRouter } from "next/router";
 import UserProfile from "../../components/UserProfile";
 import PostFeed from "../../components/PostFeed";
+
 export async function getServerSideProps({ query }) {
   const { username } = query;
 
@@ -49,6 +51,21 @@ const signOut = () => {
       router.reload();
   }
 
+
+
+  function PostList() {
+    const ref = firestore
+      .collection("users")
+      .doc(auth.currentUser.uid)
+      .collection("posts");
+    const query = ref.orderBy("createdAt");
+    const [querySnapshot] = useCollection(query); //react-firebase-hook gets relatime data
+  
+    const posts = querySnapshot?.docs.map((doc) => doc.data());
+  
+    return <PostFeed title="Your posts" posts={posts} admin />;
+  }
+  
 export default function UserProfilePage({ user, posts , PostUserUid}) 
 {
   const router = useRouter();
@@ -71,8 +88,10 @@ export default function UserProfilePage({ user, posts , PostUserUid})
           }
           >
             Logout
-          </button>
+          </button> &&
+          <PostList/>
         }
+      
       <PostFeed posts={posts} />
       <div className="spacerv-md"></div>
     </main>
